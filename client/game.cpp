670faf8a6e7b3989cf9de00e8f1f1ed31c444afa
sainath3371 <103783637+sainath3371@users.noreply.github.com>
2022-05-04 03:42:34 +0530

    Update game.cpp

--- client/game.cpp
+++ client/game.cpp
@@ -10,6 +10,12 @@ and may not be redistributed without written permission.*/
 #include <string>
 #include <iostream>
 #include <bits/stdc++.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
 #include "wall.h"
 #include "wall.cpp"
 //#include "bot.cpp"
@@ -270,7 +276,49 @@ void close()
 
 
 int main( int argc, char* args[] )
-{
+{       
+	int server_fd, new_socket, valread;
+    	struct sockaddr_in address;
+    	int opt = 1;
+    	int addrlen = sizeof(address);
+    	char buffer[1024] = { 0 };
+	string CO_OR_CLIENT;
+	
+	// Creating socket file descriptor
+    	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+	{
+        	perror("socket failed");
+        	exit(EXIT_FAILURE);
+    	}
+	
+	// Forcefully attaching socket to the port 8080
+    	if (setsockopt(server_fd, SOL_SOCKET,SO_REUSEADDR | SO_REUSEPORT, &opt,sizeof(opt))) 
+	{
+        	perror("setsockopt");
+        	exit(EXIT_FAILURE);
+    	}
+    	address.sin_family = AF_INET;
+    	address.sin_addr.s_addr = INADDR_ANY;
+    	address.sin_port = htons(PORT);
+	
+	// Forcefully attaching socket to the port 8080
+    	if (bind(server_fd, (struct sockaddr*)&address,sizeof(address)) < 0) 
+	{
+        	perror("bind failed");
+        	exit(EXIT_FAILURE);
+    	}
+    	if (listen(server_fd, 3) < 0) 
+	{
+        	perror("listen");
+        	exit(EXIT_FAILURE);
+    	}
+    	if ((new_socket = accept(server_fd, (struct sockaddr*)&address,(socklen_t*)&addrlen)) < 0) 
+	{
+        	perror("accept");
+        	exit(EXIT_FAILURE);
+    	}
+	
+	
 	//Start up SDL and create window
 	if( !init() )
 	{
@@ -433,6 +481,12 @@ int main( int argc, char* args[] )
 			}
 		}
 	}
+	char* CO_OR_SERVER = to_string(dot.getPosX()) + " " +  to_string(dot.getPosY());
+	valread = read(new_socket, buffer, 1024);
+    	
+    	send(new_socket, CO_OR_SERVER, strlen(CO_OR_SERVER), 0);
+    	printf("Hello message sent\n");
+    	return 0;
 
 	//Free resources and close SDL
 	close();
